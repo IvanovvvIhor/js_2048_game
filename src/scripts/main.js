d98b6f2bdf39d5c67c6eabc40f2da18034dca3d0
Ivanov Ihor <i.ivanovvv21122006@gmail.com>
2026-02-18 08:47:05 +0200

    new commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,9 @@
 'use strict';
 import Game from '../modules/Game.class.js';
 
+// Uncomment the next lines to use your game instance in the browser
+// const Game = require('../modules/Game.class');
+// const game = new Game();
 const game = new Game();
 
 // Write your code here
